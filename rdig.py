b9e32a7215ddfcd4cb28bf14c945423660738956
Nyxarid <nyxarid@gmail.com>
2026-02-28 17:31:07 +0200

    base

---
+++ rdig.py
@@ -0,0 +1,8 @@
+'''
+Random Drawing Idea Generator
+'''
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="A handy tool for generating random ideas for drawings and such")
